296f1cd4a1a2fc121545acd0092cb121c6b81105
MatejHyskaGit <hyska.matej@gmail.com>
2024-10-21 03:11:24 +0200

    finished webpage

--- scripts/characters.js
+++ scripts/characters.js
@@ -1,13 +1,13 @@
 let bg = document.getElementById("background")
 let btn1 = document.getElementById("charpc")
 let btn2 = document.getElementById("charnpc")
-let backbtn = document.getElementById("backbtn")
+let backbtn = document.getElementById("backbtn2")
 
 let hover1 = function(){
-    bg.style.backgroundImage = "url(../images/characters_1.png), url(../images/mech_menu.png)"
+    bg.style.backgroundImage = "url(../images/characters_1.png), url(../images/characters.png)"
 }
 let hover2 = function(){
-    bg.style.backgroundImage = "url(../images/characters_2.png), url(../images/mech_menu.png)"
+    bg.style.backgroundImage = "url(../images/characters_2.png), url(../images/characters.png)"
 }
 
 let leave = function(){
@@ -18,3 +18,13 @@ btn1.addEventListener("mouseover", hover1)
 btn1.addEventListener("mouseleave", leave)
 btn2.addEventListener("mouseover", hover2)
 btn2.addEventListener("mouseleave", leave)
+
+btn1.onclick = function(){
+    window.location.href = "/characters/playable.html"
+}
+btn2.onclick = function(){
+    window.location.href = "/characters/npc.html"
+}
+backbtn.onclick = function(){
+    window.location.href = "/menu.html"
+}
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,46 @@
+let bg = document.getElementById("background")
 let mech = document.getElementById("mech")
+let chars = document.getElementById("chars")
+let design = document.getElementById("design")
+let dlcs = document.getElementById("dlcs")
+
+let hover1 = function(){
+    bg.style.backgroundImage = "url(../images/menu_1.png), url(../images/menu.png)"
+}
+let hover2 = function(){
+    bg.style.backgroundImage = "url(../images/menu_2.png), url(../images/menu.png)"
+}
+let hover3 = function(){
+    bg.style.backgroundImage = "url(../images/menu_3.png), url(../images/menu.png)"
+}
+let hover4 = function(){
+    bg.style.backgroundImage = "url(../images/menu_4.png), url(../images/menu.png)"
+}
+
+let leave = function(){
+    bg.style.backgroundImage = "url(../images/menu.png), url(../images/menu_1.png), url(../images/menu_2.png), url(../images/menu_3.png), url(../images/menu_4.png)"
+}
+
+mech.addEventListener("mouseover", hover1)
+mech.addEventListener("mouseleave", leave)
+chars.addEventListener("mouseover", hover2)
+chars.addEventListener("mouseleave", leave)
+design.addEventListener("mouseover", hover3)
+design.addEventListener("mouseleave", leave)
+dlcs.addEventListener("mouseover", hover4)
+dlcs.addEventListener("mouseleave", leave)
+
 
 mech.onclick = function(){
     window.location.href = "/mechanics/menu.html"
 }
-document.getElementById("chars").onclick = function(){
+chars.onclick = function(){
     window.location.href = "/characters/index.html"
 }
+design.onclick = function(){
+    window.location.href = "/design/index.html"
+}
+dlcs.onclick = function(){
+    window.location.href = "/dlcs/index.html"
+}
+
