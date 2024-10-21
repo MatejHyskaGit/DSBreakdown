857729467cdfa1ebdcb5439cac4060140225d596
MatejHyskaGit <hyska.matej@gmail.com>
2024-10-21 16:07:39 +0200

    final fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 let btnmain = document.getElementById("btnmain")
 
+alert("Prosím zobrazte si stránku ve fullscreen (F11)")
+
 btnmain.onclick = function(){
     window.location.href = "/menu.html"
 }
